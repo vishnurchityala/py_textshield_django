3c4caa8acf557bbd99e1ff810a95456f9094eb3c
vishnurchityala <103362352+vishnurchityala@users.noreply.github.com>
2023-11-25 13:32:26 +0530

    Delete pytextshield directory

--- pytextshield/tool/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class encryptorForm(forms.Form):
-    plain_text = forms.CharField(max_length=3000,label="plain_text",initial="")
-    encryption_key = forms.CharField(max_length=16, label="encryption_key",initial="")
-
-class decryptorForm(forms.Form):
-    cipher_text = forms.CharField(max_length=3000,initial="")
-    decryption_key = forms.CharField(max_length=16, initial="")
--- pytextshield/tool/urls.py
+++
@@ -1,5 +0,0 @@
-from django.urls import path
-from .views import toolView
-urlpatterns = [
-    path("",toolView,name="tool"),
-]
